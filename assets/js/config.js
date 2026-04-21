eaba7719def070205f0b8877b6f4eb59ee32c22a
growthic-inc <contact@thegrowthic.com>
2026-04-21 16:20:13 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -10,9 +10,9 @@
 
 const Config = (() => {
   const SUPABASE_URL     = 'https://sagqqcctagolalfrezwg.supabase.co'
-  const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZ3FxY2N0YWdvbGFsZnJlendnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTExNjIsImV4cCI6MjA5MjMyNzE2Mn0.I6Z_XsCB9kB9XRn8ux4uP4hXQsq_WoMf9wd33vPkdFM'
 
-  if (SUPABASE_ANON_KEY === 'YOUR_SUPABASE_ANON_KEY') {
+  if (SUPABASE_ANON_KEY === 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZ3FxY2N0YWdvbGFsZnJlendnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTExNjIsImV4cCI6MjA5MjMyNzE2Mn0.I6Z_XsCB9kB9XRn8ux4uP4hXQsq_WoMf9wd33vPkdFM') {
     console.warn('[Growthic One] Supabase anon key not set. Open assets/js/config.js and add your key.')
   }
 
